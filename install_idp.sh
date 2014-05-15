9b61af086d8b577b9c07a040386178298f8dd789
Joost Damen <joost.damen@surfnet.nl>
2014-05-15 16:14:08 +0200

    added features

--- install_idp.sh
+++ install_idp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 OC_BASEDIR="/home/conext/OpenConext-vm-57"
 OC_DOMAIN="demo.openconext.org"
+. ./pass.conf
 
 if [ ! -d /opt/www/Dummy-IdP ];
 then
@@ -8,7 +9,7 @@ then
   git clone https://github.com/jmjdamen/Dummy-IdP.git
 
 	# Download and copy simpleSAMLphp
-	echo "Downloading and copying simpleSAMLphp\n"
+	echo "Downloading and copying simpleSAMLphp"
 	cd /opt/www/Dummy-IdP
 	wget https://simplesamlphp.googlecode.com/files/simplesamlphp-1.11.0.tar.gz
 	tar xzf simplesamlphp-1.11.0.tar.gz
@@ -23,11 +24,11 @@ then
 	cp -R /opt/www/Dummy-IdP/simplesamlphp_data/modules/dummy-idp /opt/www/Dummy-IdP/simplesamlphp/modules/
 
 	# Copy static files
-	echo "Copying static files\n"
+	echo "Copying static files"
 	cp /opt/www/Dummy-IdP/static/dummy-idp-logo.jpg /opt/www/OpenConext-static/media/
 
 	#copy vhost conf file, not needed when merged with OpenConext VM httpd config files
-	echo "Copying vconf\n"
+	echo "Copying vconf"
 	cp /opt/www/Dummy-IdP/httpd/dummy-idp.conf /etc/httpd/conf.d/
 fi
 
@@ -37,7 +38,7 @@ checkhost=$(grep "dummyidp" /etc/hosts | wc -l)
 if [ $checkhost -eq "0" ];
 then
         sed -i '$s/$/ dummyidp.demo.openconext.org/' /etc/hosts
-	echo "Adding host in /etc/hosts\n"
+	echo "Adding host in /etc/hosts"
 fi
 #path needed when merged with OpenConext VM
 #cat $OC_BASEDIR/configs/httpd/conf.d/dummy-idp.conf | \
@@ -50,16 +51,16 @@ fi
 # Create Dummy-IdP in Service Registry database
 # ** Enter your MySQL Root password below, remove it afterwards! **
 echo "Insert Dummy IdP entity in Service Registry"
-mysql -uroot -prootpassw0rd << eof
+mysql -uroot -p$mysqlroot_db_pass << eof
 INSERT INTO serviceregistry.janus__entity VALUES(100, 'https://dummyidp.demo.openconext.org/simplesaml/saml2/idp/metadata.php', 0, 'prodaccepted', 'saml20-idp', '', 'https://dummyidp.demo.openconext.org/simplesaml/saml2/idp/metadata.php', '', '', 'yes', 0, '', '', '', '', '', 'Added by script', 'YES')
 eof
 
 
 # Import Dummy IdP database
 echo "Creating empty database for import"
-mysql -uroot -prootpassw0rd -e "CREATE DATABASE dummyidp;CREATE USER 'sqlauth_user'@'localhost' IDENTIFIED BY 'diyp@ssw0rd';GRANT ALL ON dummyidp.* TO 'sqlauth_user'@'localhost'"
+mysql -uroot -p$mysqlroot_db_pass -e "CREATE DATABASE dummyidp;CREATE USER 'sqlauth_user'@'localhost' IDENTIFIED BY 'diyp@ssw0rd';GRANT ALL ON dummyidp.* TO 'sqlauth_user'@'localhost'"
 echo "Import Dummy IdP database"
-mysql -uroot -prootpassw0rd dummyidp < /opt/www/Dummy-IdP/db/dummy-idp-db.sql
+mysql -uroot -p$mysqlroot_db_pass dummyidp < /opt/www/Dummy-IdP/db/dummy-idp-db.sql
 
 
 
--- simplesamlphp_data/metadata/saml20-idp-hosted.php
+++ simplesamlphp_data/metadata/saml20-idp-hosted.php
@@ -2,7 +2,7 @@
 /**
  * SAML 2.0 IdP configuration for simpleSAMLphp.
  *
- * See: https://rnd.feide.no/content/idp-hosted-metadata-reference
+ * 
  */
 
 $metadata['__DYNAMIC:1__'] = array(
@@ -31,8 +31,14 @@ $metadata['__DYNAMIC:1__'] = array(
 		100 => array('class' => 'core:AttributeMap', 'name2oid'),
 	),
 	*/
+	  'certData' => 'MIICgTCCAeoCCQCbOlrWDdX7FTANBgkqhkiG9w0BAQUFADCBhDELMAkGA1UEBhMCTk8xGDAWBgNVBAgTD0FuZHJlYXMgU29sYmVyZzEMMAoGA1UEBxMDRm9vMRAwDgYDVQQKEwdVTklORVRUMRgwFgYDVQQDEw9mZWlkZS5lcmxhbmcubm8xITAfBgkqhkiG9w0BCQEWEmFuZHJlYXNAdW5pbmV0dC5ubzAeFw0wNzA2MTUxMjAxMzVaFw0wNzA4MTQxMjAxMzVaMIGEMQswCQYDVQQGEwJOTzEYMBYGA1UECBMPQW5kcmVhcyBTb2xiZXJnMQwwCgYDVQQHEwNGb28xEDAOBgNVBAoTB1VOSU5FVFQxGDAWBgNVBAMTD2ZlaWRlLmVybGFuZy5ubzEhMB8GCSqGSIb3DQEJARYSYW5kcmVhc0B1bmluZXR0Lm5vMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDivbhR7P516x/S3BqKxupQe0LONoliupiBOesCO3SHbDrl3+q9IbfnfmE04rNuMcPsIxB161TdDpIesLCn7c8aPHISKOtPlAeTZSnb8QAu7aRjZq3+PbrP5uW3TcfCGPtKTytHOge/OlJbo078dVhXQ14d1EDwXJW1rRXuUt4C8QIDAQABMA0GCSqGSIb3DQEBBQUAA4GBACDVfp86HObqY+e8BUoWQ9+VMQx1ASDohBjwOsg2WykUqRXF+dLfcUH9dWR63CtZIKFDbStNomPnQz7nbK+onygwBspVEbnHuUihZq3ZUdmumQqCw4Uvs/1Uvq3orOo/WJVhTyvLgFVK2QarQ4/67OZfHd7R+POBXhophSMv1ZOo',
+	  'redirect.sign' => false,
+	  'entityid' => 'https://dummyidp.demo.openconext.org/simplesaml/saml2/idp/metadata.php',
+	  'metadata-set' => 'saml20-idp-remote',
+	  'NameIDFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+	  'eid' => '100',  
 
-	'OrganizationURL' => 
+	  'OrganizationURL' => 
 	  array (
 	    'en' => 'https://dummyidp.demo.openconext.org',
 	  ),
@@ -44,64 +50,54 @@ $metadata['__DYNAMIC:1__'] = array(
 	  array (
 	    'en' => 'Dummy Organization',
 	  ),
-	  'name' => 
-	  array (
-	    'nl' => 'Dummy IdP ',
-	    'en' => 'Dummy IdP ',
-	  ),
-	  'logo' => 
-	  array (
-	    0 => 
-	    array (
-	      'width' => 120,
-	      'url' => 'https://static.demo.openconext.org/media/dummy-idp-logo.jpg',
-	      'height' => 60,
-	    ),
-	  ),
-	  'keywords' => 
-	  array (
-	    'nl' => 'Dummy IdP ',
-	    'en' => 'Dummy IdP ',
-	  ),
-	  'description' => 
-	  array (
-	    'nl' => 'Deze IdP bevat gebruikers voor testdoeleinden',
-	    'en' => 'This IdP contains users for test purposes',
-	  ),
-	  'contacts' => 
+
+	'ContactPerson' => 
 	  array (
 	    2 => 
 	    array (
-	      'surName' => 'IdP Support',
-	      'givenName' => 'Dummy Support',
-	      'emailAddress' => 'support@dummyidp.demo.openconext.org',
-	      'contactType' => 'support',
+	      'surName' => 'test',
+	      'givenName' => 'test',
+	      'emailAddress' => 'technician@dummyidp.demo.openconext.org',
+	      'contactType' => 'technical',
 	    ),
 	    1 => 
 	    array (
-	      'surName' => 'IdP Admin',
-	      'givenName' => 'Dummy Admin',
-	      'emailAddress' => 'admin@dummyidp.demo.openconext.org',
-	      'contactType' => 'administrative',
+	      'surName' => 'test',
+	      'givenName' => 'test',
+	      'emailAddress' => 'technician@dummyidp.demo.openconext.org',
+	      'contactType' => 'technical',
 	    ),
 	    0 => 
 	    array (
 	      'surName' => 'IdP Technician',
-	      'givenName' => 'Dummy Tech',
+	      'givenName' => 'Dummy',
 	      'emailAddress' => 'technician@dummyidp.demo.openconext.org',
 	      'contactType' => 'technical',
 	    ),
 	  ),
-	  'coin' => 
-	  array (
-	    'guest_qualifier' => 'All',
-	  ),
-	  'certData' => 'MIICgTCCAeoCCQCbOlrWDdX7FTANBgkqhkiG9w0BAQUFADCBhDELMAkGA1UEBhMCTk8xGDAWBgNVBAgTD0FuZHJlYXMgU29sYmVyZzEMMAoGA1UEBxMDRm9vMRAwDgYDVQQKEwdVTklORVRUMRgwFgYDVQQDEw9mZWlkZS5lcmxhbmcubm8xITAfBgkqhkiG9w0BCQEWEmFuZHJlYXNAdW5pbmV0dC5ubzAeFw0wNzA2MTUxMjAxMzVaFw0wNzA4MTQxMjAxMzVaMIGEMQswCQYDVQQGEwJOTzEYMBYGA1UECBMPQW5kcmVhcyBTb2xiZXJnMQwwCgYDVQQHEwNGb28xEDAOBgNVBAoTB1VOSU5FVFQxGDAWBgNVBAMTD2ZlaWRlLmVybGFuZy5ubzEhMB8GCSqGSIb3DQEJARYSYW5kcmVhc0B1bmluZXR0Lm5vMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDivbhR7P516x/S3BqKxupQe0LONoliupiBOesCO3SHbDrl3+q9IbfnfmE04rNuMcPsIxB161TdDpIesLCn7c8aPHISKOtPlAeTZSnb8QAu7aRjZq3+PbrP5uW3TcfCGPtKTytHOge/OlJbo078dVhXQ14d1EDwXJW1rRXuUt4C8QIDAQABMA0GCSqGSIb3DQEBBQUAA4GBACDVfp86HObqY+e8BUoWQ9+VMQx1ASDohBjwOsg2WykUqRXF+dLfcUH9dWR63CtZIKFDbStNomPnQz7nbK+onygwBspVEbnHuUihZq3ZUdmumQqCw4Uvs/1Uvq3orOo/WJVhTyvLgFVK2QarQ4/67OZfHd7R+POBXhophSMv1ZOo',
-	  'redirect.sign' => false,
-	  'entityid' => 'https://dummyidp.demo.openconext.org/simplesaml/saml2/idp/metadata.php',
-	  'metadata-set' => 'saml20-idp-remote',
-	  'NameIDFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
-	  'eid' => '100',    
 
+	  /* This metadata needs to be placed in (this) correct order, otherwise it wont be imported in service registry! */
+	  'UIInfo' => array(
+	  'DisplayName' => array(
+		'en' => 'Dummy IdP',
+		 'nl' => 'Dummy IdP',
+	     ),
+	     'Description' => array(
+		 'en' => 'Test IdP that contains Dummy users, ideal for test purposes',
+	     ),
+	     'Keywords' => array(
+		 'en' => array('dummy','idp','opencxxonext'),
+		 'nl' => array('dummy','idp','openconext'),
+	     ),
+	     'Logo' => array(
+		 array(
+		     'url'    => 'https://static.demo.openconext.org/media/dummy-idp-logo.jpg',
+		     'height' => 60,
+		     'width'  => 120,
+		 ),
+	     ),
+
+
+),
 
 );
